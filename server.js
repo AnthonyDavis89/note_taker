ecbc590af1745327d805dd9b6c66ef9927d1b1a3
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-05-01 14:40:11 -0400

    fixing files

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const fs = require("fs");
 
+let notes = require("./db.json");
+
+// starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening" + PORT);
+});
 
 // start up express app to handle data parsing
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname, "/public"));
 
 // HTML get requests
 app.get("/", function (req, res) {
@@ -18,13 +24,23 @@ app.get("/", function (req, res) {
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
-
-  // app.get("/webCss", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "Develop/public/assets/css/styles.css"));
-  // });
 });
 
-// starts the server to begin listening
-app.listen(PORT, function () {
-    console.log("App listening" + PORT);
+app.get("/api/notes", function (req,res){
+  res.sendFile(notes);
+});
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", (req,res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
+    if (err) {
+      throw err;
+    }
+    res.json(notes);
   });
+});
