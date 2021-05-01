73299ede72630a6a62d5208ad86d2386e38d03d7
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-05-01 15:17:30 -0400

    updating files

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/api/notes", (req,res) => {
+app.post("/api/notes", (req,res) => {
   const newNote = req.body;
   notes.push(newNote);
   fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
