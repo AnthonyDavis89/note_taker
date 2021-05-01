062d7afaf477b3fab68d649498e61a3e7eff768b
Anthony Davis <Anthony_Davis89@icloud.comgit config  --global user.email git config  --global user.email Anthony_Davis89@icloud.comgit config  --global user.email Anthony_Davis89@icloud.com>
2021-05-01 15:06:48 -0400

    fixed dirname in server.js file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.listen(PORT, function () {
 // start up express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname, "/public"));
+app.use(express.static(__dirname + "/public"));
 
 // HTML get requests
 app.get("/", function (req, res) {
